b22a6cb4e69b0a41a89806e7fa86cd2f534b8e22
minho0391 <minh05@naver.com>
2026-07-16 10:19:09 +0900

    Refactor BarChart configuration into a separate variable for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,26 @@ import { BarChart } from "@d3plus/react";
 
 function App() {
   const data = [10, 20, 35, 18, 42, 30, 55];
+  const barData = [
+    { id: "alpha", x: 4, y: 7 },
+    { id: "alpha", x: 5, y: 25 },
+    { id: "alpha", x: 6, y: 13 },
+    { id: "beta", x: 4, y: 17 },
+    { id: "beta", x: 5, y: 8 },
+    { id: "beta", x: 6, y: 13 },
+  ];
+
+  const barConfig = {
+    data: barData,
+    groupBy: "id",
+    x: "x",
+    y: "y",
+  };
   return (
     <>
       <h1>D3 chart</h1>
       <LineChart data={data} />
-      <BarChart
-        config={{
-          data: [
-            { id: "alpha", x: 4, y: 7 },
-            { id: "alpha", x: 5, y: 25 },
-            { id: "alpha", x: 6, y: 13 },
-            { id: "beta", x: 4, y: 17 },
-            { id: "beta", x: 5, y: 8 },
-            { id: "beta", x: 6, y: 13 },
-          ],
-          groupBy: "id",
-          x: "x",
-          y: "y",
-        }}
-      />
+      <BarChart config={barConfig} />
     </>
   );
 }
